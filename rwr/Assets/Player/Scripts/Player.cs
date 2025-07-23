94bb24779dfee6174b9c8e2e24e9dc2f542bb788
Keegan Ritchie <keegan.ritchie0@gmail.com>
2025-07-23 12:53:50 -0600

    Added code to change sprite.

--- rwr/Assets/Player/Scripts/Player.cs
+++ rwr/Assets/Player/Scripts/Player.cs
@@ -1,18 +1,23 @@
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    SpriteRenderer spriteRenderer;
+    public Sprite[] spriteSheetSprites;
+    public SpriteRenderer spriteRenderer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        spriteSheetSprites = Resources.LoadAll<Sprite>("SDV_Dog");
+        
         spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.GetComponent<SpriteRenderer>().sprite = spriteSheetSprites[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        spriteRenderer.sprite = SDV_Dog_1;
+        
     }
 }
