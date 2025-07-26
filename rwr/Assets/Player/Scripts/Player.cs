1fbbca25cd535bc2f91b2418d5f639a391707128
Keegan Ritchie <keegan.ritchie0@gmail.com>
2025-07-26 13:08:01 -0600

    Adding animation controller.

--- rwr/Assets/Player/Scripts/Player.cs
+++ rwr/Assets/Player/Scripts/Player.cs
@@ -3,21 +3,55 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Sprite[] spriteSheetSprites;
+    /* --- Public Variables --- */
+    public Rigidbody2D rigidBody;
+    public Animator animator;
+    
     public SpriteRenderer spriteRenderer;
+    public float velocity = 2.0f;
+    
+    /* --- Private Variables --- */
+    private int[] _indexWalkingDown = new[] { 0, 1, 2, 3 };
+    private int[] _indexWalkingRight = new[] { 4, 5, 6, 7 };
+    private int[] _indexWalkingUp = new[] { 8, 9, 10, 11 };
+    private int[] _indexWalkingLeft = new[] { 12, 13, 14, 15 };
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector2 _direction;
+
     void Start()
     {
-        spriteSheetSprites = Resources.LoadAll<Sprite>("SDV_Dog");
-        
         spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.GetComponent<SpriteRenderer>().sprite = spriteSheetSprites[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateDirection();
     }
+
+    private void UpdateDirection()
+    {
+        _direction.x = Input.GetAxisRaw("Horizontal");
+        Debug.Log(_direction.x);
+        _direction.y = Input.GetAxisRaw("Vertical");
+        Debug.Log(_direction.y);
+    }
+    
+    void UpdateSprite()
+    {
+        int framesPerSecond = 20;
+        int n_frame = (int)(Time.time * framesPerSecond);
+        if (Input.GetKey("w"))
+        {
+            spriteRenderer.GetComponent<SpriteRenderer>().sprite = spriteSheetSprites[0];
+        }
+    }
+    
+    void UpdateAnimator()
+    {
+        animator.SetFloat("Horizontal", _direction.x);
+        animator.SetFloat("Vertical", _direction.y);
+        animator.SetFloat("Speed", _direction.sqrMagnitude);
+    }
+    
 }
